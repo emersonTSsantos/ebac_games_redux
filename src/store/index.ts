8cc16a88b2280ea29266308e7378130e0684fcb2
Emerson Teixeira <emerson@exemplo.com>
2024-08-20 19:14:55 -0300

    Criando testes para o componente Header

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState
+} from '@reduxjs/toolkit'
 
 import carrinhoReducer from './reducers/carrinho'
 
-export const store = configureStore({
-  reducer: {
-    carrinho: carrinhoReducer
-  }
+import api from '../services/api'
+
+const rootReducer = combineReducers({
+  carrinho: carrinhoReducer,
+  [api.reducerPath]: api.reducer
 })
 
-export type RootReducer = ReturnType<typeof store.getState>
+export function configuraStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
+    preloadedState
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof configuraStore>
